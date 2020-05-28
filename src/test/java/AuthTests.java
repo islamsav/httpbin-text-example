d5e80ec4d656b0ffebd1c971f21ad5a0dc67c378
islamsav <Dagstyle05!!>
2020-05-28 23:59:49 +0400

    Добавил 2 негативных теста

--- src/test/java/AuthTests.java
+++ src/test/java/AuthTests.java
@@ -6,17 +6,13 @@ import org.testng.annotations.Test;
 import pojo.AuthModel;
 import pojo.EndPoints;
 
-//TODO динамик дата сделать через okHttp3
-//TODO добавить негативные сценарии
-//TODO добавить спецификацию
 public class AuthTests {
 
     @Test(testName = "POSITIVE auth test on basic-auth/ with random data", description = "execute GET request on http://httpbin.org/basic-auth/{user}/{password}", suiteName = "auth")
-    public void performBasicAuthRequest() {
-        String userName = Utils.getRandomString(Utils.EN, 10) + Utils.rnd(3, 5);
-        String password = Utils.rnd(3, 5) + Utils.getRandomString(Utils.EN, 10);
-        AuthModel authModel = RestAssured
-                .given()
+    public void positivePerformBasicAuthRequest() {
+        String userName = Utils.getRandomString(Utils.EN, 10);
+        String password = Utils.getRandomString(Utils.RAND, 10);
+        AuthModel authModel = RestAssured.given()
                 .pathParam("user", userName)
                 .pathParam("passwd", password)
                 .auth()
@@ -32,11 +28,26 @@ public class AuthTests {
         Assert.assertEquals(userName, authModel.getUser());
     }
 
-    @Test(testName = "POSITIVE auth test on bearer/ with random data", description = "execute GET request on http://httpbin.org/bearer/")
+    @Test(testName = "NEGATIVE auth test on basic-auth/ with random data", description = "execute GET request on http://httpbin.org/basic-auth/{user}/{password}", suiteName = "auth")
+    public void negativePerformBasicAuthRequest() {
+        String userName = Utils.getRandomString(Utils.EN, 10);
+        String password = Utils.getRandomString(Utils.RAND, 10);
+        int code = RestAssured.given()
+                .pathParam("user", userName)
+                .pathParam("passwd", password)
+                .auth()
+                .basic("!@#$%^&*()", "!@#$%^&*()")
+                .contentType(ContentType.JSON)
+                .get(EndPoints.BASIC_AUTH)
+                .then().log().status().log().body()
+                .extract().statusCode();
+        Assert.assertEquals(401, code, ErrorMessages.STATUS_CODE_NOT_MATCH);
+    }
+
+    @Test(testName = "POSITIVE auth test on bearer/ with random data", description = "execute GET request on http://httpbin.org/bearer/", suiteName = "bearer")
     public void performBearerAuthRequest() {
-        String generateBearer = Utils.getRandomString(Utils.EN, 10) + Utils.rnd(5000, 10000);
-        AuthModel authModel = RestAssured
-                .given()
+        String generateBearer = Utils.getRandomString(Utils.EN, 10) + Utils.getRandomString(Utils.DIGIT, 30);
+        AuthModel authModel = RestAssured.given()
                 .header("Authorization", "Bearer " + generateBearer)
                 .contentType(ContentType.JSON)
                 .get(EndPoints.BEARER)
@@ -47,5 +58,16 @@ public class AuthTests {
         Assert.assertNotNull(authModel, String.format(ErrorMessages.UNSUCCESSFUL_MAPPING, "AuthModel"));
         Assert.assertTrue(authModel.getAuthenticated());
         Assert.assertEquals(generateBearer, authModel.getToken());
+    }
+
+    @Test(testName = "NEGATIVE auth test on bearer/ without header", description = "execute GET request on http://httpbin.org/bearer/", suiteName = "bearer")
+    public void negativeBearerAuthRequest() {
+        // Тут не передаем обязательный хидер для данного запроса и получаем статус код 401
+        int code = RestAssured.given()
+                .contentType(ContentType.JSON)
+                .get(EndPoints.BEARER)
+                .then().log().status().log().body()
+                .extract().statusCode();
+        Assert.assertEquals(401, code, ErrorMessages.STATUS_CODE_NOT_MATCH);
     }
 }
