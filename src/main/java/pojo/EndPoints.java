335ca8f741857ad9ffb30832c7afcdbb97b8aa0a
islamsav <Dagstyle05!!>
2020-05-29 00:42:00 +0400

    Позитивный и негативный тест по digest-auth

--- src/main/java/pojo/EndPoints.java
+++ src/main/java/pojo/EndPoints.java
@@ -4,4 +4,5 @@ public class EndPoints {
     public static final String HTTP_BIN_MAIN_URL = "http://httpbin.org/";
     public static final String BASIC_AUTH = HTTP_BIN_MAIN_URL + "basic-auth/{user}/{passwd}";
     public static final String BEARER = HTTP_BIN_MAIN_URL + "bearer";
+    public static final String DIGEST_AUTH = HTTP_BIN_MAIN_URL + "digest-auth/{qop}/{user}/{passwd}";
 }
--- src/test/java/AuthTests.java
+++ src/test/java/AuthTests.java
@@ -8,7 +8,7 @@ import pojo.EndPoints;
 
 public class AuthTests {
 
-    @Test(testName = "POSITIVE auth test on basic-auth/ with random data", description = "execute GET request on http://httpbin.org/basic-auth/{user}/{password}", suiteName = "auth")
+    @Test(testName = "POSITIVE auth test on /basic-auth/{user}/{password} with random data", suiteName = "auth")
     public void positivePerformBasicAuthRequest() {
         String userName = Utils.getRandomString(Utils.EN, 10);
         String password = Utils.getRandomString(Utils.RAND, 10);
@@ -28,7 +28,7 @@ public class AuthTests {
         Assert.assertEquals(userName, authModel.getUser());
     }
 
-    @Test(testName = "NEGATIVE auth test on basic-auth/ with random data", description = "execute GET request on http://httpbin.org/basic-auth/{user}/{password}", suiteName = "auth")
+    @Test(testName = "NEGATIVE auth test on /basic-auth/{user}/{password} with random data", suiteName = "auth")
     public void negativePerformBasicAuthRequest() {
         String userName = Utils.getRandomString(Utils.EN, 10);
         String password = Utils.getRandomString(Utils.RAND, 10);
@@ -44,7 +44,7 @@ public class AuthTests {
         Assert.assertEquals(401, code, ErrorMessages.STATUS_CODE_NOT_MATCH);
     }
 
-    @Test(testName = "POSITIVE auth test on bearer/ with random data", description = "execute GET request on http://httpbin.org/bearer/", suiteName = "bearer")
+    @Test(testName = "POSITIVE auth test on bearer/ with random data", suiteName = "bearer")
     public void performBearerAuthRequest() {
         String generateBearer = Utils.getRandomString(Utils.EN, 10) + Utils.getRandomString(Utils.DIGIT, 30);
         AuthModel authModel = RestAssured.given()
@@ -60,12 +60,46 @@ public class AuthTests {
         Assert.assertEquals(generateBearer, authModel.getToken());
     }
 
-    @Test(testName = "NEGATIVE auth test on bearer/ without header", description = "execute GET request on http://httpbin.org/bearer/", suiteName = "bearer")
+    @Test(testName = "NEGATIVE auth test on bearer/ without header", suiteName = "bearer")
     public void negativeBearerAuthRequest() {
         // Тут не передаем обязательный хидер для данного запроса и получаем статус код 401
         int code = RestAssured.given()
                 .contentType(ContentType.JSON)
                 .get(EndPoints.BEARER)
+                .then().log().status().log().body()
+                .extract().statusCode();
+        Assert.assertEquals(401, code, ErrorMessages.STATUS_CODE_NOT_MATCH);
+    }
+
+    @Test(testName = "POSITIVE /digest-auth/{qop}/{user}/{passwd}", suiteName = "digest-auth")
+    public void positiveDigestAuthRequest() {
+        String userName = Utils.getRandomString(Utils.EN, 10);
+        String password = Utils.getRandomString(Utils.RAND, 10);
+        AuthModel authModel = RestAssured.given()
+                .pathParam("user", userName)
+                .pathParam("passwd", password)
+                .pathParam("qop", "auth")
+                .contentType(ContentType.JSON)
+                .auth().digest(userName, password)
+                .when().get(EndPoints.DIGEST_AUTH)
+                .then().log().status().log().body()
+                .statusCode(HttpStatus.SC_OK)
+                .extract().as(AuthModel.class);
+        Assert.assertTrue(authModel.getAuthenticated());
+        Assert.assertEquals(userName, authModel.getUser());
+    }
+
+    @Test(testName = "NEGATIVE /digest-auth/{qop}/{user}/{passwd}", suiteName = "digest-auth")
+    public void negativeDigestAuthRequest() {
+        String userName = Utils.getRandomString(Utils.EN, 10);
+        String password = Utils.getRandomString(Utils.RAND, 10);
+        int code = RestAssured.given()
+                .pathParam("user", userName)
+                .pathParam("passwd", password)
+                .pathParam("qop", "auth")
+                .contentType(ContentType.JSON)
+                .auth().digest(userName, userName)
+                .when().get(EndPoints.DIGEST_AUTH)
                 .then().log().status().log().body()
                 .extract().statusCode();
         Assert.assertEquals(401, code, ErrorMessages.STATUS_CODE_NOT_MATCH);
