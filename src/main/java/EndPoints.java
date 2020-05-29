893d93c0aa81c047516eacddc730addb3ac7dbcf
islamsav <varta91@ya.ru>
2020-05-29 16:58:07 +0300

    Переместил Endpoint

--- src/main/java/EndPoints.java
+++ src/main/java/EndPoints.java
@@ -5,4 +5,7 @@ public class EndPoints {
     public static final String BEARER = BASE + "bearer";
     public static final String DIGEST_AUTH = BASE + "digest-auth/{qop}/{user}/{passwd}";
     public static final String DIGEST_AUTH_WITH_ALGORITHM = BASE + "digest-auth/{qop}/{user}/{passwd}/{algorithm}";
+    public static final String DIGEST_AUTH_WITH_ALGORITHM_AND_STALE_AFTER = BASE + "digest-auth/{qop}/{user}/{passwd}/{algorithm}/{stale_after}";
+    public static final String HIDDEN_BASIC_AUTH = BASE + "hidden-basic-auth/{user}/{passwd}";
+
 }
--- src/test/java/AuthTests.java
+++ src/test/java/AuthTests.java
@@ -1,13 +1,16 @@
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.apache.http.HttpStatus;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pojo.AuthModel;
 
+import static org.hamcrest.Matchers.equalTo;
+
 public class AuthTests {
 
-    @Test(testName = "POSITIVE auth test on /basic-auth/{user}/{password} with random data", suiteName = "auth")
+    @Test(testName = "POSITIVE auth test on /basic-auth/{user}/{password} with random data", suiteName = "basic-auth")
     public void positivePerformBasicAuthRequest() {
         String userName = Utils.getRandomString(Utils.EN, 10);
         String password = Utils.getRandomString(Utils.RAND, 10);
@@ -27,7 +30,7 @@ public class AuthTests {
         Assert.assertEquals(userName, authModel.getUser());
     }
 
-    @Test(testName = "NEGATIVE auth test on /basic-auth/{user}/{password} with random data", suiteName = "auth")
+    @Test(testName = "NEGATIVE auth test on /basic-auth/{user}/{password} with random data", suiteName = "basic-auth")
     public void negativePerformBasicAuthRequest() {
         String userName = Utils.getRandomString(Utils.EN, 10);
         String password = Utils.getRandomString(Utils.RAND, 10);
@@ -84,6 +87,7 @@ public class AuthTests {
                 .then().log().status().log().body()
                 .statusCode(HttpStatus.SC_OK)
                 .extract().as(AuthModel.class);
+        Assert.assertNotNull(authModel, String.format(ErrorMessages.UNSUCCESSFUL_MAPPING, "AuthModel"));
         Assert.assertTrue(authModel.getAuthenticated());
         Assert.assertEquals(userName, authModel.getUser());
     }
@@ -119,6 +123,7 @@ public class AuthTests {
                 .then().log().status().log().body()
                 .statusCode(HttpStatus.SC_OK)
                 .extract().as(AuthModel.class);
+        Assert.assertNotNull(authModel, String.format(ErrorMessages.UNSUCCESSFUL_MAPPING, "AuthModel"));
         Assert.assertTrue(authModel.getAuthenticated());
         Assert.assertEquals(userName, authModel.getUser());
     }
@@ -128,9 +133,10 @@ public class AuthTests {
         String userName = Utils.getRandomString(Utils.EN, 10);
         String password = Utils.getRandomString(Utils.RAND, 10);
         int code = RestAssured.given()
+                .pathParam("qop", "auth")
                 .pathParam("user", userName)
                 .pathParam("passwd", password)
-                .pathParam("qop", "auth")
+                .pathParam("algorithm", "MD5")
                 .pathParam("algorithm", "MD5")
                 .contentType(ContentType.JSON)
                 .auth().digest(userName, userName)
@@ -138,5 +144,56 @@ public class AuthTests {
                 .then().log().status().log().body()
                 .extract().statusCode();
         Assert.assertEquals(401, code, ErrorMessages.STATUS_CODE_NOT_MATCH);
+    }
+
+    @Test(testName = "POSITIVE digest-auth/{qop}/{user}/{passwd}/{algorithm}/{stale_after}", suiteName = "digest-auth")
+    public void positiveDigestAuthWithStaleAfter() {
+        String userName = Utils.getRandomString(Utils.EN, 10);
+        String password = Utils.getRandomString(Utils.RAND, 10);
+        Response response = RestAssured.given()
+                .pathParam("qop", "auth")
+                .pathParam("user", userName)
+                .pathParam("passwd", password)
+                .pathParam("algorithm", "MD5")
+                .pathParam("stale_after", "never")
+                .contentType(ContentType.JSON).auth().digest(userName, password)
+                .when().get(EndPoints.DIGEST_AUTH_WITH_ALGORITHM_AND_STALE_AFTER);
+        response.then().log().status().log().body();
+        int code = response.statusCode();
+        Assert.assertEquals(200, code, ErrorMessages.STATUS_CODE_NOT_MATCH);
+        response.then().body("authenticated", equalTo(true), "user", equalTo(userName));
+    }
+
+    @Test(testName = "NEGATIVE digest-auth/{qop}/{user}/{passwd}/{algorithm}/{stale_after}", suiteName = "digest-auth")
+    public void negativeDigestAuthWithStaleAfter() {
+        String userName = Utils.getRandomString(Utils.EN, 10);
+        String password = Utils.getRandomString(Utils.RAND, 10);
+        RestAssured.given()
+                .pathParam("qop", "auth")
+                .pathParam("user", userName)
+                .pathParam("passwd", password)
+                .pathParam("algorithm", "MD5")
+                .pathParam("stale_after", "never")
+                .contentType(ContentType.JSON)
+                .auth().digest("!@#$%^&*()", "!@#$%^&*()")
+                .when().get(EndPoints.DIGEST_AUTH_WITH_ALGORITHM_AND_STALE_AFTER)
+                .then().log().status().log().body()
+                .assertThat()
+                .statusCode(HttpStatus.SC_UNAUTHORIZED);
+    }
+
+    @Test
+    public void test1() {
+        String userName = Utils.getRandomString(Utils.EN, 10);
+        String password = Utils.getRandomString(Utils.RAND, 10);
+        RestAssured.given()
+                .auth().basic(userName, password)
+                .pathParam("user", userName)
+                .pathParam("passwd", password)
+                .contentType(ContentType.JSON)
+                .when().get(EndPoints.HIDDEN_BASIC_AUTH)
+                .then()
+                .statusCode(HttpStatus.SC_OK);
+
     }
 }
